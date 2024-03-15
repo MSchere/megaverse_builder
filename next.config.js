3f5dc4fdf8eece69a17b6a250f0ac5d3b063ee4c
MSchere <manu_schere@proton.me>
2024-03-15 03:46:39 +0100

    Delete unused stuff

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-import { env } from "./src/env.js";
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
@@ -8,15 +7,7 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/fastapi/:path*",
-        destination: `${env.NEXT_PUBLIC_API_URL}/:path*`,
-      },
-    ];
-  },
+    reactStrictMode: true,
 };
 
 export default config;
